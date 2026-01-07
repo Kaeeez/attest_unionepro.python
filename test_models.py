5939be82dbc2fcc8694c6c912b9822c4185b61b1
Slava <Slava@yandex.ru>
2026-01-07 22:24:16 +0300

    Добавлен class TestTask

--- test_models.py
+++ test_models.py
@@ -2,7 +2,7 @@ import unittest
 from decimal import Decimal
 from models import Employee, Task, Project, IN_PROGRESS, DONE_STATUS
 
-c(unittest.TestCase):
+class TestEmployee(unittest.TestCase):
     def test_add_hours_positive(self):
         emp = Employee(id=1, name='Иван', position='разработчик', salary=Decimal('100000'))
         emp.add_hours(Decimal('8'))
@@ -14,3 +14,20 @@ c(unittest.TestCase):
         emp = Employee(id=2, name='Мария', position='тестировщик', salary=Decimal('90000'))
         with self.assertRaises(ValueError):
             emp.add_hours(Decimal('-5'))
+
+class TestTask(unittest.TestCase):
+    def test_str_done(self):
+        task = Task(id=1, title='Выполнить задачу', description='Тестовая задача', status=DONE_STATUS)
+        expected = "✓ Выполнить задачу"
+        self.assertEqual(task.str(), expected)
+
+    def test_str_in_progress(self):
+        task = Task(id=2, title='Сделать отчет', description='Тест2', status=IN_PROGRESS)
+        expected = "◷ Сделать отчет"
+        self.assertEqual(task.str(), expected)
+
+    def test_project_employee_id(self):
+        task = Task(id=3, title='Вторая', description='...', status=IN_PROGRESS, project_id=10, employee_id=20)
+        self.assertEqual(task.project_id, 10)
+        self.assertEqual(task.employee_id, 20)
+
